6f2af3a64dc185cc553fd37cafd488af42f7165e
Ben Alman <cowboy@rj3.net>
2014-11-27 08:43:37 -0500

    Process multiple markets in-order. Skip dupes.

--- process/process.sh
+++ process/process.sh
@@ -5,35 +5,56 @@
 img_dir=/vagrant/images
 out_dir=/vagrant/output
 tmp_dir=$out_dir/tmp
-
-[[ -d "$out_dir" ]] || mkdir -p "$out_dir"
-rm -rf "$out_dir"/*
-mkdir -p "$tmp_dir"
-
-# Based on 1920x1200
-crop_geometry="1181x715+98+310"
-
-cd "$img_dir"
-images=(*.bmp)
-cd "$out_dir"
+proc_file=$out_dir/processed.txt
 
 function header() { echo "== $@ =="; }
 
+# Get all not-previously-processed images.
+cd "$img_dir"
+shopt -s extglob nullglob
+p="$(cat "$proc_file" 2>/dev/null | tr "\n" "|")"
+images=(!($p).bmp)
+
+if (( ${#images[@]} == 0 )); then
+  echo "No new screenshots to process!"
+  exit
+fi
+
+# Create output directory
+[[ -d "$out_dir" ]] || mkdir -p "$out_dir"
+cd "$out_dir"
+# Cleanup temp directory
+[[ -d "$tmp_dir" ]] && rm -rf "$tmp_dir"
+mkdir -p "$tmp_dir"
+
 header "Cropping"
+crop_geometry="1181x715+98+310" # Based on 1920x1200
 for f in "${images[@]}"; do
   echo "Cropping $f"
   convert "$img_dir/$f" -crop $crop_geometry "$tmp_dir/$f"
 done
 
 header "Joining"
-joined="Joined.png"
-echo "Copying ${images[0]} to $joined"
-cp "$tmp_dir/${images[0]}" $joined
-slice_height=30
-for f in "${images[@]:1}"; do
-  convert "$tmp_dir/$f" -crop x${slice_height}+0+0 "$tmp_dir/slice.png"
-  search="$(compare -metric RMSE -subimage-search $joined "$tmp_dir/slice.png" "$tmp_dir/search.png" 2>&1)"
+function join_new_image() {
+  ((joined_i++))
+  joined="$2-joined.png"
+  echo "Copying $2 to $joined"
+  cp "$tmp_dir/$2.bmp" $joined
+}
+function join_next_image() {
+  local slice_height=30
+  local src="$tmp_dir/$2.bmp"
+  local slice="$tmp_dir/slice$1.png"
+  convert "$src" -crop x${slice_height}+0+0 "$slice"
+  search="$(compare -metric RMSE -subimage-search $joined "$slice" "$tmp_dir/search.png" 2>&1)"
   offset=$(echo "$search" | sed 's/.*,//')
-  echo "$f slice found at ${offset}px in $joined ($search)"
-  convert $joined -page +0+$offset "$tmp_dir/$f" -layers mosaic $joined
+  (( $offset == 0 )) && return 1
+  echo " $2 slice matched at $offset ($search)"
+  convert $joined -page +0+$offset "$src" -layers mosaic $joined
+}
+
+for i in "${!images[@]}"; do
+  img=$(basename ${images[$i]} .bmp)
+  (( $i > 0 )) && join_next_image $i $img || join_new_image $i $img
+  echo $img >> "$proc_file"
 done
